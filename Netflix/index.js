6da2b7f18e3d950e19cdf55e9a39f6598406adcf
Rijushree007 <60056104+Rijushree123@users.noreply.github.com>
2021-07-02 13:52:23 +0530

    Update index.js

--- Netflix/index.js
+++ Netflix/index.js
@@ -35,3 +35,32 @@ ReactDOM.render(
    </>,
     document.getElementById('root')
 );
+
+
+
+                                                                                //OR
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Cards.jsx';
+import './index.css';
+import Sdata from './Sdata';
+
+
+ReactDOM.render(
+   <> 
+   <h1 className="heading_style">List of Top 5 Netflix Series in 2020</h1>
+    {Sdata.map((val,index)=>{
+          console.log(index);
+      return (<Card 
+      imgsrc={val.imgsrc}
+      title={val.title}
+      sname={val.sname}
+      link={val.link}
+    />)
+    })}
+   </>,
+    document.getElementById('root')
+);
