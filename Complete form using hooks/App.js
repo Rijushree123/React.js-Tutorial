412a1c2dbb4a253f6c710a9648db855d7b5eb8c5
Rijushree007 <60056104+Rijushree123@users.noreply.github.com>
2021-07-05 02:42:20 +0530

    Add files via upload

---
+++ Complete form using hooks/App.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react'
+
+
+
+
+
+const App=()=>{
+    
+    const[fullName,setFullName] = useState({
+        fname:'',
+        lname:'',
+        email:'',
+        phone:''
+    });
+    
+
+    const inputEvent=(event)=>{
+        console.log(event.target.value);
+        console.log(event.target.name);
+        //console.log(event.target.placeholder);
+        //const value = event.target.value;
+        //const name = event.target.name;
+        const {value,name} = event.target;
+
+        setFullName((preValue)=>{
+            //console.log(preValue);
+            if(name==='fname')
+            {
+                return{
+                    fname: value,
+                    lname: preValue.lname,
+                    email: preValue.email,
+                    phone: preValue.phone,
+                };
+            }
+            else if(name === 'lname'){
+                return{
+                    fname: preValue.fname,
+                    lname: value,
+                    email: preValue.email,
+                    phone: preValue.phone,
+                    };
+            }
+            else if(name==='email')
+            {
+                return{
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: value,
+                    phone: preValue.phone,
+                };
+            }
+            else if(name === 'phone'){
+                return{
+                    fname: preValue.lname,
+                    lname: preValue.lname,
+                    email: preValue.email,
+                    phone: value,
+                    };
+            }
+        
+        });
+    }
+
+    const onSubmits = (event) =>{
+        event.preventDefault();
+        alert('form submitted!');
+    }
+    
+    
+
+
+    return (
+        <>
+        <div className="main_div">
+            <form onSubmit={onSubmits}>
+                <div>
+                    <h1>Hello {fullName.fname} {fullName.lname}</h1>
+                    <p>{fullName.email}</p>
+                    <p>{fullName.phone}</p>
+                    <br/>
+                    <input type="text" 
+                    placeholder="Enter Your First Name"
+                    name='fname'
+                    onChange={inputEvent} value={fullName.fname}></input>
+                    <br/>
+                    <input type="text" 
+                    placeholder="Enter Your Last Name"  
+                    name='lname'
+                    onChange={inputEvent} value={fullName.lname}></input>
+                    <br></br>
+                    <input type="email" 
+                    placeholder="Enter Your Email"
+                    name='email'
+                    onChange={inputEvent} value={fullName.email}></input>
+                    <br/>
+                    <input type="number" 
+                    placeholder="Enter Your Last Password"  
+                    name='phone'
+                    onChange={inputEvent} value={fullName.phone}></input>
+                    <button type='submit' onClick={onSubmits}>Submit Me 👍</button> 
+                </div>
+            </form>
+        </div>
+        </>
+    );
+};
+export default App;
