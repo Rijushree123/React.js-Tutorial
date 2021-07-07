70340bc32a8d20f70b0f29fc660082cb729498f1
Rijushree007 <60056104+Rijushree123@users.noreply.github.com>
2021-07-08 03:49:15 +0530

    Add files via upload

---
+++ npm digital clock/ToDoList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Clock from 'react-digital-clock';
+
+const ToDoList=()=>{
+    return (
+        <div className='main_div' style={{backgroundColor:'blue', fontSize:'50px'}}>
+        <Clock/>
+        </div>
+        )
+};
+export default ToDoList;
